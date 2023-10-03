50ad37380dbcc6c90f15198bb303881430e75402
Chris Brown <chrisbrown@sageworksstudio.com>
2023-10-03 11:35:12 -0700

    Initial commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,11 @@
+<script setup>
+import USMap from "../components/US-Map.vue";
+</script>
+
+<template>
+  <q-layout view="hHh lpR fFf">
+    <q-page-container>
+      <USMap />
+    </q-page-container>
+  </q-layout>
+</template>
